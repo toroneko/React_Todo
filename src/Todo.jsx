ce5a69e761d9cc807d48c37382de694c4e382ab2
toroneko <167988065+toroneko@users.noreply.github.com>
2024-06-02 04:47:48 +0000

    CSSでスタイリング

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,35 +3,43 @@ import "./styles.css";
 export const Todo = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="Todoを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTodo</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTodo</p>
         <ul>
           <li>
-            <p>Todoです</p>
-            <button>完了</button>
-            <button>削除</button>
+            <div className="list-row">
+              <p className="todo-item">Todoです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
           </li>
           <li>
-            <p>Todoです</p>
-            <button>完了</button>
-            <button>削除</button>
+            <div className="list-row">
+              <p className="todo-item">Todoです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
           </li>
         </ul>
       </div>
-      <div>
-        <p>完了のTodo</p>
+      <div className="complete-area">
+        <p className="title">完了のTodo</p>
         <ul>
           <li>
-            <p>Todoでした</p>
-            <button>戻す</button>
+            <div className="list-row">
+              <p className="todo-item">Todoでした</p>
+              <button>戻す</button>
+            </div>
           </li>
           <li>
-            <p>Todoでした</p>
-            <button>戻す</button>{" "}
+            <div className="list-row">
+              <p className="todo-item">Todoでした</p>
+              <button>戻す</button>{" "}
+            </div>
           </li>
         </ul>
       </div>
